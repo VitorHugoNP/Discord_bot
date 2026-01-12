a77179465903d2254f3f9197116fd2faa7d9932f
VitorHugoNP <supimpayey@gmail.com>
2026-01-12 18:14:13 -0300

    Fix: Dados funcionam com ou sem os turnos (#)

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ async def benzer(ctx):
     await ctx.send("✝️ O CHAT TA BENZIDO! ✝️")
     
 @bot.command()
-async def roll(ctx, dice: str, turns = 1):
+async def roll(ctx, dice: str):
     try:
-        turns, dice_part = dice.lower().split("#")
-        turns = int(turns)
-        
-        rolls_str, limit_str = dice_part.lower().split("d")
+        if "#" in dice:
+            turns, dice_part = dice.lower().split("#")
+            turns = int(turns)
+            rolls_str, limit_str = dice_part.lower().split("d")
+        else:
+            rolls_str,limit_str = dice.lower().split("d")
+            turns = int(1)
         rolls = int(rolls_str)
         limit = int(limit_str)
 
     except ValueError:
-        await ctx.send(f"Use o formato correto: `{bot.intents}roll 2d6`")
+        await ctx.send(f"Use o formato correto como: `.roll 2d6` ou `.roll 2#2d6`")
         return
 
     
@@ -42,7 +45,7 @@ async def roll(ctx, dice: str, turns = 1):
                 if chance == 1:
                     roll = random.randint(1, limit)
                 else:
-                    await ctx.send("‼️ Uma mágica Maligna foi jogada sobre este dado... Não foi possivel abençoa-la 😭")
+                    await ctx.send("* ‼️ Uma mágica Maligna foi jogada sobre este dado... Não foi possivel abençoa-la 😭 *")
             results.append(roll)
     
         total = sum(results)
